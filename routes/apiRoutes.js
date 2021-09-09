fe319ffc012326ac15f181ca5bd9408470e440c4
George Ngo <georgengo@msn.com>
2021-09-08 18:41:27 -0700

    added delete and post routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,10 +16,31 @@ router.post("/notes", (req, res) => {
     fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8")
   );
   // set id based on what the next index of the array will be
-  req.body.id = animals.length.toString();
+  req.body.id = notes.length.toString();
+  notes.push(req.body);
+  //write file sync to the db/db.json file 
+  fs.writeFileSync(
+    path.join(__dirname, "../db/db.json"),
+    JSON.stringify(notes),
+    "utf8"
+  );
+  res.json(notes);
+});
 
-  //write file sync to the db/db.json file and yay done
-  
+// DELETE /api/notes/:id
+router.delete("/notes/:id", (req, res) => {
+  const notes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8")
+  );
+  const newNotes = notes.filter((note) => note.id !== req.params.id);
+
+  //write file sync to the db/db.json file 
+  fs.writeFileSync(
+    path.join(__dirname, "../db/db.json"),
+    JSON.stringify(newNotes),
+    "utf8"
+  );
+  res.json(newNotes);
 });
 
 module.exports = router;
